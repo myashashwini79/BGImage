7b0e15655c8c215fd78bfc00985cd6faed820c48
myashashwini79 <myashashwini79@gmail.com>
2025-01-31 20:32:32 +0530

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-let images=document.getElementsByTagName("img");
-for(let i=0;i< images.length;i++) {
-    images[i].addEventListener('click',()=> {
-        document.querySelector('body').style.cssText =`background-image:url("${images[i].src}");
-        backlground-repeat: no-repeat;
-        background-size: cover; `;
-    });
-}
